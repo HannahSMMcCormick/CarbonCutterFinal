c89999117f253a47ed5e556105b47ef2a90f69e1
caitlinthaeler <114182834+caitlinthaeler@users.noreply.github.com>
2024-02-25 00:07:59 +0000

    added data fetch function

---
+++ lib/datasets.dart
@@ -0,0 +1,37 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<Map<String, dynamic>> fetchProductData(keywords) async {
+  final String url = 'https://amazon-pricing-and-product-info.p.rapidapi.com/';
+  final Map<String, String> queryParams = {
+    'keywords': keywords,
+    'domain': 'de',
+  };
+  final Map<String, String> headers = {
+    'X-RapidAPI-Key': '78ba902980msh98339935fad99adp12706fjsn4cb7c296161d',
+    'X-RapidAPI-Host': 'amazon-pricing-and-product-info.p.rapidapi.com',
+  };
+
+  final Uri uri = Uri.parse(url).replace(queryParameters: queryParams);
+
+  try {
+    final response = await http.get(uri, headers: headers);
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+      return responseData;
+    } else {
+      print('Failed to load data: ${response.statusCode}');
+      return {}
+    }
+  } catch (error) {
+    print('Error: $error');
+    return {}
+  }
+}
+
+void main(){
+    final keywords = 'purse';
+    final productData = await fetchProductData(keywords);
+    print(productData);
+}
+
